c68b8ed3ef944ff7ed6133e855be1fdfa5933ad7
Leandro Loureiro <leandroloureiro@protonmail.com>
2019-12-25 01:57:34 +0000

    Updates

---
+++ precache-manifest.47328d6526a282469ea5556cdd731dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a974a4bb647e643b750fd3977fc4f2f0",
+    "url": "/mortgage-calculator-web/index.html"
+  },
+  {
+    "revision": "8ccd5eec08333fef7930",
+    "url": "/mortgage-calculator-web/static/css/2.f5298653.chunk.css"
+  },
+  {
+    "revision": "8ccd5eec08333fef7930",
+    "url": "/mortgage-calculator-web/static/js/2.26d34a1a.chunk.js"
+  },
+  {
+    "revision": "41dec1805a1138a227a108b795866cf1",
+    "url": "/mortgage-calculator-web/static/js/2.26d34a1a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "703eb19fcde810c9af43",
+    "url": "/mortgage-calculator-web/static/js/main.9502f9e1.chunk.js"
+  },
+  {
+    "revision": "85534cdb7713077b8b1f",
+    "url": "/mortgage-calculator-web/static/js/runtime-main.1002f932.js"
+  }
+]);
